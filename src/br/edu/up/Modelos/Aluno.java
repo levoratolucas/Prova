081fb42d7288698545be5e88d9889b2b4d77e481
Lucas Levorato <141377688+levoratolucas@users.noreply.github.com>
2024-06-28 20:13:11 -0300

    Add files via upload

---
+++ src/br/edu/up/Modelos/Aluno.java
@@ -0,0 +1,27 @@
+package br.edu.up.Modelos;
+
+public class Aluno {
+
+    private String matricula;
+    private String nome;
+    private double nota;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getNota() {
+        return nota;
+    }
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
